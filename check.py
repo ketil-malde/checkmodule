2c98f74cacb93dcc66d51090f628e84c28a98109
Ketil Malde <ketil@malde.org>
2021-09-28 13:37:23 +0200

    Instantiate objects to check them

--- check.py
+++ check.py
@@ -15,12 +15,15 @@ def error(dir, str):
     global ERR
     print('\033[91mERROR: '+dir+': \033[0m'+str)
     ERR+=1
-    
+
+import sys
+
 def check_module(dir):
     '''Verify that the python module exists, and go to next check'''
     files = os.listdir(dir)
     if 'README.md' not in files:
         warn(dir, 'README is missing')
+    sys.path.insert(1, dir)
     if 'Data.py' in files:
         check_data_class(dir)
     elif 'Model.py' in files:
@@ -30,18 +33,29 @@ def check_module(dir):
     else:
         error(dir,'does not seem to be a component')
         print(files)
-        
+    sys.path.remove(dir)
+
 def check_data_class(dir):
     '''Check properties of the Data object'''
     print('Found data component: ',dir)
-    
+    import Data
+    d = Data.Data()
+    print(dir(d))
+    #check that d has get() and validate()
+
 def check_model_class(dir):
     '''Check properties of the Model object'''
     print('Found model component: ',dir)
+    import Model
+    m = Model.Model()
+    print(dir(m))
 
 def check_project_class(dir):
     '''Check properties of the Project object'''
     print('Found project component: ', dir)
+    import Project
+    p = Project.Project()
+    print(dir(p))
 
 # something something __main__
 
@@ -50,7 +64,9 @@ modules = [
     'git@git.imr.no:endrem/imr_ml_datastore.git'
     ]
 
-for repo in modules:
+def check_repo(repo):
+    global WARN
+    global ERR
     W=WARN
     E=ERR
     dirname = os.path.basename(repo) # does this work?
@@ -59,5 +75,9 @@ for repo in modules:
     if W==WARN and E==ERR:
         print(f'\033[92m{dirname}:\033[0m no warning or errors')
 
+for repo in modules:
+    check_repo(repo)
+    print('--------------------')
+
 print(f'\nTotal number of warnings: {WARN}\nTotal number of errors: {ERR}')
-    
+
